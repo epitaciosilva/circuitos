586596d28357c32830580e7e0aa0b321caa7dc8b
Epitácio <bessa.epitacio>
2019-09-12 18:28:25 -0300

    Finalizando comparador com o loop

--- comparador/comparador.vhd
+++ comparador/comparador.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity comparador is
 	port(
 		A, B : in std_logic_vector(4 downto 0);
-		X, Y, Z : out std_logic
+		ig, me, ma : out std_logic
 	);
 end comparador;
 
@@ -13,47 +13,22 @@ architecture comparar of comparador is
 	
 	begin
 		
-		igual(4) <= '1';
-		menor(4) <= '0';
-		maior(4) <= '0';
-		
 		process(A,B)
 			begin
-				for I in 4 to 1 loop
-					igual(I-1) <= igual(I) and (not(A(I) xor B(I)));
-					maior(I-1) <= maior(I) or (igual(I) and (A(I) and (not B(I))));
-					menor(I-1) <= menor(I) or (igual(I) and ((not A(I)) and B(I)));
+				for I in 0 to 4 loop
+					if (I = 0) then 
+						igual(I) <= '1' and (not(A(4-I) xor B(4-I)));
+						maior(I) <= maior(4-I) or ('1' and (A(4-I) and (not B(4-I))));
+						menor(I) <= menor(4-I) or ('1' and ((not A(4-I)) and B(4-I)));
+					elsif (I = 4) then
+						ig <= igual(I-1) and (not(A(4-I) xor B(4-I)));
+						ma <= maior(I-1) or (igual(I-1) and (A(4-I) and (not B(4-I))));
+						me <= menor(I-1) or (igual(I-1) and ((not A(4-I)) and B(4-I)));
+					else
+						igual(I) <= igual(I-1) and (not(A(4-I) xor B(4-I)));
+						maior(I) <= maior(I-1) or (igual(I-1) and (A(4-I) and (not B(4-I))));
+						menor(I) <= menor(I-1) or (igual(I-1) and ((not A(4-I)) and B(4-I)));
+					end if;
 				end loop;
-				x <= igual(0) and (not(A(0) xor B(0)));
-				y <= maior(0) or (igual(0) and (A(0) and (not B(0))));
-				z <= menor(0) or (igual(0) and ((not A(0)) and B(0)));
 		end process;
-		
-		
-		
---		igual(3) <= igual(4) and (not(A(4) xor B(4)));
---		maior(3) <= maior(4) or (igual(4) and (A(4) and (not B(4))));
---		menor(3) <= menor(4) or (igual(4) and ((not A(4)) and B(4)));
---		
---		igual(2) <= igual(3) and (not(A(3) xor B(3)));
---		maior(2) <= maior(3) or (igual(3) and (A(3) and (not B(3))));
---		menor(2) <= menor(3) or (igual(3) and ((not A(3)) and B(3)));
---		
---		igual(1) <= igual(2) and (not(A(2) xor B(2)));
---		maior(1) <= maior(2) or (igual(2) and (A(2) and (not B(2))));
---		menor(1) <= menor(2) or (igual(2) and ((not A(2)) and B(2)));
---		
---		
---		igual(0) <= igual(1) and (not(A(1) xor B(1)));
---		maior(0) <= maior(1) or (igual(1) and (A(1) and (not B(1))));
---		menor(0) <= menor(1) or (igual(1) and ((not A(1)) and B(1)));
---
---		
---		x <= igual(0) and (not(A(0) xor B(0)));
---		y <= maior(0) or (igual(0) and (A(0) and (not B(0))));
---		z <= menor(0) or (igual(0) and ((not A(0)) and B(0)));
-		
---		X <= igual(4);
---		Y <= maior(4);
---		Z <= menor(4);
 end comparar;
