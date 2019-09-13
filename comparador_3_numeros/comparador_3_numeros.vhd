281e5bd6fc450c5f4a427856118a3df685ba6bae
Epitácio <bessa.epitacio@gmail.com>
2019-09-12 23:40:53 -0300

    Comparador entre 3 números de 8 bits com simulação exigida

---
+++ comparador_3_numeros/comparador_3_numeros.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity comparador_3_numeros is
+	port(
+		num1, num2, num3 : in std_logic_vector(7 downto 0);
+		maior : out std_logic_vector(7 downto 0)
+	);
+end comparador_3_numeros;
+
+architecture comparar of comparador_3_numeros is 
+	begin 
+		process(num1,num2,num3) 
+			begin 
+				if(num1 > num2 and num1 > num3) then 
+					maior <= num1;
+				elsif(num2 > num1 and num2 > num3) then
+					maior <= num2;
+				else
+					maior <= num3;
+				end if;
+		end process;
+end comparar;	
+				
